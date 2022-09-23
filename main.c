74c1ff57cb8366cc398ae4d2e38aecc4fbe2b60f
somrat <ahmedsomrat945@gmail.com>
2022-09-24 00:14:50 +0600

    left and right shift done

--- main.c
+++ main.c
@@ -1,35 +1,80 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int m = 11, n = 22;
-    char  a[m][n];
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(i == 0 || j == 0 || i == n-1 || j == n - 1 || i == 10)
-            a[i][j]= '*'; 
+    char a[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1 || i == 10)
+                a[i][j] = '*';
             else if (i == 5 && j == 5)
             {
                 a[i][j] = 'P';
             }
-            else if (i == 9 && j == 12){
+            else if (i == 9 && j == 12)
+            {
                 a[i][j] = 'G';
             }
             else
-            a[i][j] = ' ';
-           
-                
+                a[i][j] = ' ';
         }
-        printf("\n");
-        
     }
-    for(int i = 0; i < 11; i++){
-        for(int j = 0; j < 22; j++){
-          printf("%c", a[i][j]);
-                
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 22; j++)
+        {
+            printf("%c", a[i][j]);
         }
         printf("\n");
-        
     }
 
+    // printf("Enter 'D' to right \n");
+    // char c ;
+    // scanf("%c", &c);
+
+    // if(c == 'd'){
+    // for (int i = 0; i < 11; i++)
+    // {
+    //     for (int j = 0; j < 22; j++)
+    //     {   
+    //       if(a[i][j] == 'P'){
+    //         a[i][j] = ' ';
+    //         j++;
+    //         a[i][j] = 'P';
+    //       }
+           
+    //     }
+    // }
+    // }
+    printf("Enter 'A' to left \n");
+    char c;
+    scanf("%c", &c);
+
+    if(c == 'a'){
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 22; j++)
+        {   
+          if(a[i][j] == 'P'){
+            a[i][j] = ' ';
+            j--;
+            a[i][j] = 'P';
+          }
+           
+        }
+    }
+
+    }
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 22; j++)
+        {
+            printf("%c", a[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
