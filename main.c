ad166069fdbcde798efd01e2a68153f7e55b0b8a
MD Fahim Abrar Saikat <fahimabrar.saikat@gmail.com>
2022-09-27 08:32:48 +0600

    Test Commit: Formatted main.c in standard C syntax

--- main.c
+++ main.c
@@ -4,54 +4,51 @@
 
 int main()
 {
-  int m = 11, n = 22;
-  
-  char ** a = malloc(sizeof(char*) * m);
+    int m = 11, n = 22;
 
-  for(int i = 0; i < m; i++){
-    a[i] = malloc(n);
-  }
+    char ** a = malloc(sizeof(char*) * m);
 
-
-
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      if (i == 0 || j == 0 || i == n - 1 || j == n - 1 || i == 10)
-        a[i][j] = '*';
-      else if (i == 5 && j == 5)
-      {
-        a[i][j] = 'P';
-      }
-      else if (i == 9 && j == 12)
-      {
-        a[i][j] = 'G';
-      }
-      else
-        a[i][j] = 'x';
+    for(int i = 0; i < m; i++){
+        a[i] = malloc(n);
     }
-  }
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
+
+    for (int i = 0; i < m; i++)
     {
-      printf("%c", a[i][j]);
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1 || i == 10)
+                a[i][j] = '*';
+            else if (i == 5 && j == 5)
+            {
+                a[i][j] = 'P';
+            }
+            else if (i == 9 && j == 12)
+            {
+                a[i][j] = 'G';
+            }
+            else
+                a[i][j] = 'x';
+        }
     }
-    printf("\n");
-  }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%c", a[i][j]);
+        }
+        printf("\n");
+    }
 
-  while (1)
-  {
-    printf("Enter 'D' to right \nEnter 'A' to left \nEnter 'w' to up \nEnter 's' to down \n");
-    char c;
-    scanf("%c", &c);
+    while (1)
+    {
+        printf("Enter 'D' to right \nEnter 'A' to left \nEnter 'w' to up \nEnter 's' to down \n");
+        char c;
+        scanf("%c", &c);
 
-    map(m, n, a, c);
+        map(m, n, a, c);
+    }
 
-  }
-  
-  free(a);
-  return 0;
+    free(a);
+    return 0;
 }
 
